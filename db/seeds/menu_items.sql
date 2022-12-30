17e1dc3465dde41252cb14ce92b258fc4261c615
Amina Malik <amina.malik@telus.com>
2022-12-30 02:10:24 -0500

    formatting

--- db/seeds/menu_items.sql
+++ db/seeds/menu_items.sql
@@ -1,32 +1,28 @@
-  1 INSERT INTO menu_items (id, name, price, description, thumbnail_photo_url)
-  2 VALUES 
- (3, 'Vegetarian', $15.99, 'Mushroom, Tomato, Green Pepper, Red Onion, Cheese & Tomato Sauce', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…gie-lovers.48ba54d49ff2257671521b477967633a.1.jpg'),
- (4, 'Hawaiian', $17.99, 'Ham, Tropical Sauce, Cheddar, Bacon, Cheese & Tomato Sauce, Pineapple', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…a.hawaiian.6c049fb1897c6f45f597dd1671fbd016.1.jpg'),
- (5, 'Margherita', $17.99, 'Pesto marinated tomatoes, parmesan and pizza mozzarella', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…margherita.aa693f6fa624bef4e7d69c63b9f40d12.1.jpg'),
- (6, 'Supreme', $18.99, 'Pepperoni, mild sausage, beef topping, green pepper, sliced mushrooms, red onion and pizza mozzarella', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…eme-lovers.3706cdc20b0752ac212c0d68a310fb18.1.jpg'),
- (7, 'BBQ chicken', $17.99, 'BBQ sauce, seasoned grilled chicken breast, red onion, green peppers, smoked aged cheddar and pizza mozzarella', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…bq-chicken.fca67baad9e87b442f101c1cc26e3ae2.1.jpg'),
- (8, 'Pepperoni', $13.99, 'Pepperoni, mozzarella', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…oni-lovers.69f7bdf7b6f50a87eb2886934fe0be9f.1.jpg'),
- (9, 'Cheese', $12.99, 'Mozarella cheese', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…ese-lovers.21455ca09617c38f3618f2b525dfed42.1.jpg'),
- (10, 'Mushroom', $13.99, 'sliced mushroom and pizza mozzarella', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…an-classic.ad7765e7d0bfed6e00b09053416cba63.1.jpg'),
-
-
-
-
-
+   INSERT INTO menu_items (id, name, price, description, thumbnail_photo_url)
+   VALUES 
+ ('Vegetarian', $15.99, 'Mushroom, Tomato, Green Pepper, Red Onion, Cheese & Tomato Sauce', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…gie-lovers.48ba54d49ff2257671521b477967633a.1.jpg'),
+ ('Hawaiian', $17.99, 'Ham, Tropical Sauce, Cheddar, Bacon, Cheese & Tomato Sauce, Pineapple', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…a.hawaiian.6c049fb1897c6f45f597dd1671fbd016.1.jpg'),
+ ('Margherita', $17.99, 'Pesto marinated tomatoes, parmesan and pizza mozzarella', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…margherita.aa693f6fa624bef4e7d69c63b9f40d12.1.jpg'),
+ ('Supreme', $18.99, 'Pepperoni, mild sausage, beef topping, green pepper, sliced mushrooms, red onion and pizza mozzarella', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…eme-lovers.3706cdc20b0752ac212c0d68a310fb18.1.jpg'),
+ ('BBQ chicken', $17.99, 'BBQ sauce, seasoned grilled chicken breast, red onion, green peppers, smoked aged cheddar and pizza mozzarella', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…bq-chicken.fca67baad9e87b442f101c1cc26e3ae2.1.jpg'),
+ ('Pepperoni', $13.99, 'Pepperoni, mozzarella', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…oni-lovers.69f7bdf7b6f50a87eb2886934fe0be9f.1.jpg'),
+ ('Cheese', $12.99, 'Mozarella cheese', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…ese-lovers.21455ca09617c38f3618f2b525dfed42.1.jpg'),
+ ('Mushroom', $13.99, 'sliced mushroom and pizza mozzarella', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…an-classic.ad7765e7d0bfed6e00b09053416cba63.1.jpg'),
+ 
 
 /* sides
 */
 
- (11, 'BBQ Wings', $15.99, 'https://d21klxpge3tttg.cloudfront.net/wp-content/u…/01/featured-honey-soy-chicken-wings-1024x640.jpg'),
- (12, 'Hot Wings', $15.99, 'https://d21klxpge3tttg.cloudfront.net/wp-content/u…/01/featured-honey-soy-chicken-wings-1024x640.jpg'),
- (13, 'LightHouse Cheesy Bread', $9.99, 'https://cache.dominos.com/nolo/ca/en/6_94_15/assets/build/market/CA/_en/images/img/products/larges/F_CHEBRD.jpg'),
+ ('BBQ Wings', $15.99, 'https://d21klxpge3tttg.cloudfront.net/wp-content/u…/01/featured-honey-soy-chicken-wings-1024x640.jpg'),
+ ('Hot Wings', $15.99, 'https://d21klxpge3tttg.cloudfront.net/wp-content/u…/01/featured-honey-soy-chicken-wings-1024x640.jpg'),
+ ('LightHouse Cheesy Bread', $9.99, 'https://cache.dominos.com/nolo/ca/en/6_94_15/assets/build/market/CA/_en/images/img/products/larges/F_CHEBRD.jpg'),
 
 /* drinks
 */
 
- (14, 'Pepsi 591ml Bottle', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…rink.pepsi.b71560da143bf3da099dcc1d9017ef01.1.jpg'),
- (15, '7up 591ml Bottle', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…/drink.7up.7a1925da523094943917270945a29b76.1.jpg'),
- (16, 'Bottle Aquafina', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…quafina-2A.d09136e18ceb2f175b7249aca4aa85cf.1.jpg'),
+ ('Pepsi 591ml Bottle', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…rink.pepsi.b71560da143bf3da099dcc1d9017ef01.1.jpg'),
+ ('7up 591ml Bottle', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…/drink.7up.7a1925da523094943917270945a29b76.1.jpg'),
+ ('Bottle Aquafina', 'https://api.pizzahut.io/v1/content/en-ca/ca-1/imag…quafina-2A.d09136e18ceb2f175b7249aca4aa85cf.1.jpg'),
 
 
 
